d5eb89766524c1c8c087fd30893572c0d82034c3
shaoyu0966 <b06705028@ntu.edu.tw>
2019-10-05 15:51:03 +0800

    part_c encapsule everything into class 'MyRobot'

--- part_c.cpp
+++ part_c.cpp
@@ -1,32 +1,49 @@
 # include "Aria.h"
 # include <iostream>
-
 # define DELTA_HEADING 20
 # define MOVE_STEP 600
 
-class ControlHandler
+/* ------------------------------ CONTROL_HANDLER ------------------------------ */
+
+class MyRobot
 {
 private:
+	ArRobot robot;
+	ArSonarDevice sonar;
+	ArSimpleConnector connector;
 	ArKeyHandler keyHandler;
 	ArFunctor1C<ArRobot, double> leftRotate;
 	ArFunctor1C<ArRobot, double> rightRotate;
     ArFunctor1C<ArRobot, double> forward;
 	ArFunctor1C<ArRobot, double> backward;
-	ArFunctorC<ArRobot> stop;
+
 public:
-	ControlHandler(ArRobot& robot): leftRotate(robot, &ArRobot::setDeltaHeading, DELTA_HEADING),
-									rightRotate(robot, &ArRobot::setDeltaHeading, -DELTA_HEADING),
-									forward(robot, &ArRobot::move, MOVE_STEP),
-									backward(robot, &ArRobot::move, -MOVE_STEP) {}
-	void addKeyControl(ArRobot& robot) {
+	MyRobot():	leftRotate(robot, &ArRobot::setDeltaHeading, DELTA_HEADING),
+				rightRotate(robot, &ArRobot::setDeltaHeading, -DELTA_HEADING),
+				forward(robot, &ArRobot::move, MOVE_STEP),
+				backward(robot, &ArRobot::move, -MOVE_STEP)
+	{
+		robot.addRangeDevice(&sonar);
+		if (!connector.connectRobot(&robot)){
+			printf("Could not connect to robot... exiting\n");
+			Aria::shutdown();
+			Aria::exit(1);
+		}
+		robot.comInt(ArCommands::ENABLE, 1);
+		robot.runAsync(false);
+	}
+
+	void addKeyControl() {
+		robot.lock()
 		Aria::setKeyHandler(&keyHandler);
 		robot.attachKeyHandler(&keyHandler);
 		keyHandler.addKeyHandler(ArKeyHandler::LEFT, &leftRotate);
 		keyHandler.addKeyHandler(ArKeyHandler::RIGHT, &rightRotate);
 		keyHandler.addKeyHandler(ArKeyHandler::UP, &forward);
 		keyHandler.addKeyHandler(ArKeyHandler::DOWN, &backward);
-		keyHandler.addKeyHandler(ArKeyHandler::SPACE, &stop);
+		robot.unlock();
 	}
+
 	void printControlMsg() {
 		printf("You may press escape to exit.\n");
 		printf("Press LEFT to left-rotate.\n");
@@ -34,33 +51,27 @@ public:
 		printf("Press UP to move forward.\n");
 		printf("Press DOWN to move backward.\n");	
 	}
+
+	void getX() { return robot.getX(); }
+	void getY() { return robot.getY(); }
+	void getTh() { return robot.getTh(); }
+	void getVel() { return robot.getVel(); }
+	void getRotVel() { return robot.getRotVel(); }
 };
+
+/* ------------------------------ MAIN ------------------------------ */
 
 int main(int argc, char **argv)
 {
-	ArRobot robot;
-	ArSonarDevice sonar;
-	ArSimpleConnector connector(&argc,argv);
-
-	robot.addRangeDevice(&sonar);
 	Aria::init();
-	if (!connector.connectRobot(&robot)){
-		printf("Could not connect to robot... exiting\n");
-		Aria::shutdown();
-		Aria::exit(1);
-	}
-	robot.comInt(ArCommands::ENABLE, 1);
-	robot.runAsync(false);
-
-	// TODO: control the robot
-	robot.lock();
-	ControlHandler control_handler(robot);
-	control_handler.addKeyControl(robot);
-	control_handler.printControlMsg();	
-	robot.unlock();
+	
+	MyRobot robot();
+	robot.addKeyControl();
+	robot.printControlMsg();
 
 	while(true){
 		printf("%f %f %f %f %f\n", robot.getX(), robot.getY(), robot.getTh(), robot.getVel(), robot.getRotVel());
+		// printf("%f %f %f\n", sonar.currentReadingPolar(0, 180), sonar.currentReadingPolar(-180, 0), sonar.currentReadingPolar(-45, 45));
 		ArUtil::sleep(300);
 	}
 
