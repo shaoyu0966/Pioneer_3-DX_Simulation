818f69ed0cba2096e0758912521efd0e6c5d9fd2
shaoyu0966 <b06705028@ntu.edu.tw>
2019-10-06 22:19:44 +0800

    able to move to the target position (no final rotation)

---
+++ bonus.cpp
@@ -0,0 +1,168 @@
+# include "Aria.h"
+# include <iostream>
+# include <string>
+# include <algorithm>
+# define DELTA_HEADING 20
+# define MOVE_STEP 600
+
+/* ------------------------------ CONTROL_HANDLER ------------------------------ */
+
+class MyRobot
+{
+private:
+	ArRobot robot;
+	ArSonarDevice sonar;
+	ArSimpleConnector connector;
+	ArKeyHandler keyHandler;
+	ArFunctor1C<ArRobot, double> leftRotate;
+	ArFunctor1C<ArRobot, double> rightRotate;
+    // ArFunctor1C<ArRobot, double> forward;
+	// ArFunctor1C<ArRobot, double> backward;
+	ArFunctorC<MyRobot> safe_forward;
+	ArFunctorC<MyRobot> safe_backward;
+
+public:
+	MyRobot(int* argc, char** argv):
+				connector(argc, argv),
+				leftRotate(robot, &ArRobot::setDeltaHeading, DELTA_HEADING),
+				rightRotate(robot, &ArRobot::setDeltaHeading, -DELTA_HEADING),
+				// forward(robot, &ArRobot::move, MOVE_STEP),
+				// backward(robot, &ArRobot::move, -MOVE_STEP),
+				safe_forward(*this, &MyRobot::safeForward),
+				safe_backward(*this, &MyRobot::safeBackward)
+	{
+		robot.addRangeDevice(&sonar);
+		if (!connector.connectRobot(&robot)){
+			printf("Could not connect to robot... exiting\n");
+			Aria::shutdown();
+			Aria::exit(1);
+		}
+		robot.comInt(ArCommands::ENABLE, 1);
+		robot.runAsync(false);
+	}
+
+	void addKeyControl() {
+		robot.lock();
+		Aria::setKeyHandler(&keyHandler);
+		robot.attachKeyHandler(&keyHandler);
+		keyHandler.addKeyHandler(ArKeyHandler::LEFT, &leftRotate);
+		keyHandler.addKeyHandler(ArKeyHandler::RIGHT, &rightRotate);
+		// keyHandler.addKeyHandler(ArKeyHandler::UP, &forward);
+		// keyHandler.addKeyHandler(ArKeyHandler::DOWN, &backward);
+		keyHandler.addKeyHandler(ArKeyHandler::UP, &safe_forward);
+		keyHandler.addKeyHandler(ArKeyHandler::DOWN, &safe_backward);
+		robot.unlock();
+	}
+
+	void printControlMsg() {
+		printf("You may press escape to exit.\n");
+		printf("Press LEFT to left-rotate.\n");
+		printf("Press RIGHT to left-rotate.\n");
+		printf("Press UP to move forward.\n");
+		printf("Press DOWN to move backward.\n");	
+	}
+
+	double getX() { return robot.getX(); }
+	double getY() { return robot.getY(); }
+	double getTh() { return robot.getTh(); }
+	double getVel() { return robot.getVel(); }
+	double getRotVel() { return robot.getRotVel(); }
+    double getSonarReading(int start_angle, int end_angle) { return sonar.currentReadingPolar(start_angle, end_angle); }
+    ArPose getPose() { return robot.getPose(); }
+	
+	void safeForward() {
+        robot.lock();
+		double range = sonar.currentReadingPolar(-45, 45);
+		robot.move(range >= 5 * MOVE_STEP? MOVE_STEP : range * 0.2);
+        robot.unlock();
+	}
+	void safeBackward() {
+        robot.lock();
+		double range = sonar.currentReadingPolar(135, 225);
+		robot.move(range >= 6 * MOVE_STEP? -MOVE_STEP : range * (-0.1));
+        robot.unlock();
+	}
+
+    double safeRange() {
+        return (sonar.currentReadingPolar(-45, 45) - 1.5 * robot.getRobotLengthFront());
+    }
+
+    void moveTo(double x, double y, double rad) {
+
+		ArPose target(x, y, ArMath::radToDeg(rad));
+        double front_range = 0;
+        double move_angle = 0;
+        double move_distance = 0;
+        double left_range = 0, right_range = 0;
+        
+        while(true){
+                       
+            move_angle = robot.getPose().findAngleTo(target);
+            move_distance = robot.findDistanceTo(target);
+
+            robot.lock();
+            robot.setHeading(move_angle);
+            robot.unlock();
+            while(!robot.isHeadingDone()) ;
+            front_range = getSonarReading(-30, 30);
+
+            if(front_range >= move_distance) {
+                robot.lock();
+                robot.move(move_distance);
+                robot.unlock();
+                while(!robot.isMoveDone()) ;
+                // rotate
+                break;
+            }
+
+            robot.lock();
+            robot.move(std::min(front_range * 0.75, safeRange()));
+            robot.unlock();
+            while(!robot.isMoveDone()) ;
+            left_range = getSonarReading(60, 120);
+            right_range = getSonarReading(-120, -60);
+            
+            robot.lock();
+            robot.setHeading(robot.getTh() + 90 * (left_range > right_range? 1 : (-1)));
+            robot.unlock();
+            while(!robot.isHeadingDone()) ;
+
+            robot.lock();
+            robot.move(std::min(std::max(left_range, right_range) * 0.5, safeRange()));
+            robot.unlock();
+            while(!robot.isMoveDone()) ;
+            
+        }
+
+    }
+};
+
+/* ------------------------------ MAIN ------------------------------ */
+
+int main(int argc, char **argv)
+{	
+    double target_x = 0, target_y = 0, target_th = 0;
+	std::cout << "Please type your target X, Y, and Theta(in radian):" << std::endl;
+	std::cin >> target_x >> target_y >> target_th;
+
+	Aria::init();
+	MyRobot robot(&argc, argv);
+	robot.addKeyControl();
+	// robot.printControlMsg();
+	
+	robot.moveTo(target_x, target_y, target_th);
+
+	for(int i = 0; i >= 0; i++){
+
+		if(!(i % 10)){
+			std::cout << std::string(54, '-') << std::endl;
+			printf("%10s %10s %10s %10s %10s\n", "X", "Y", "Theta", "Vel", "RotVel");
+		}
+
+		printf("%10.2f %10.2f %10.2f %10.2f %10.2f\n", robot.getX(), robot.getY(), robot.getTh(), robot.getVel(), robot.getRotVel());
+		ArUtil::sleep(300);
+	}
+
+	Aria::shutdown();
+	Aria::exit(0);
+}
