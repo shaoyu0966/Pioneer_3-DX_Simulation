80d883870e710bed958f3b91d3cd0f8ee4ab7900
shaoyu0966 <b06705028@ntu.edu.tw>
2019-10-06 18:18:53 +0800

    part_e able to move to the target given with stdin

--- part_e.cpp
+++ part_e.cpp
@@ -67,38 +67,73 @@ public:
 	double getTh() { return robot.getTh(); }
 	double getVel() { return robot.getVel(); }
 	double getRotVel() { return robot.getRotVel(); }
+    double getSonarReading(int start_angle, int end_angle) { return sonar.currentReadingPolar(start_angle, end_angle); }
+    ArPose getPose() { return robot.getPose(); }
 	
 	void safeForward() {
+        robot.lock();
 		double range = sonar.currentReadingPolar(-45, 45);
 		robot.move(range >= 5 * MOVE_STEP? MOVE_STEP : range * 0.2);
+        robot.unlock();
 	}
 	void safeBackward() {
+        robot.lock();
 		double range = sonar.currentReadingPolar(135, 225);
 		robot.move(range >= 6 * MOVE_STEP? -MOVE_STEP : range * (-0.1));
+        robot.unlock();
 	}
-};
 
+    void moveTo(double x, double y, double rad) {
+		std::cout << "angle in degree: " << ArMath::radToDeg(rad) << std::endl;
+        ArPose target(x, y, ArMath::radToDeg(rad));
+        double move_angle = robot.getPose().findAngleTo(target);
+        double move_distance = robot.findDistanceTo(target);
+        std::cout << "move angle: " << move_angle << std::endl;
+        std::cout << "move distance: " << move_distance << std::endl;
+        
+        robot.lock();
+        robot.setHeading(move_angle);
+        robot.unlock();
+		
+		while(!robot.isHeadingDone()) ;
+		robot.lock();
+		robot.move(move_distance);
+		robot.unlock();
+
+		while(!robot.isMoveDone()) ;
+		robot.lock();
+		robot.setHeading(ArMath::radToDeg(rad));
+		robot.unlock();
+
+		while(!robot.isHeadingDone()) ;
+    }
+};
 
 /* ------------------------------ MAIN ------------------------------ */
 
 int main(int argc, char **argv)
-{
+{	
+    double target_x = 0, target_y = 0, target_th = 0;
+	std::cout << "Please type your target X, Y, and Theta(in radian):" << std::endl;
+	std::cin >> target_x >> target_y >> target_th;
+
 	Aria::init();
-	
 	MyRobot robot(&argc, argv);
 	robot.addKeyControl();
-	robot.printControlMsg();
+	// robot.printControlMsg();
+	
+	robot.moveTo(target_x, target_y, target_th);
 
-	for(int i = 0; i >= 0; i++){
-        
-		if(!(i % 10)){
-			std::cout << std::string(54, '-') << std::endl;
-			printf("%10s %10s %10s %10s %10s\n", "X", "Y", "Theta", "Vel", "RotVel");
-		}
+	// for(int i = 0; i >= 0; i++){
 
-		printf("%10.2f %10.2f %10.2f %10.2f %10.2f\n", robot.getX(), robot.getY(), robot.getTh(), robot.getVel(), robot.getRotVel());
-		ArUtil::sleep(300);
-	}
+	// 	if(!(i % 10)){
+	// 		std::cout << std::string(54, '-') << std::endl;
+	// 		printf("%10s %10s %10s %10s %10s\n", "X", "Y", "Theta", "Vel", "RotVel");
+	// 	}
+
+	// 	printf("%10.2f %10.2f %10.2f %10.2f %10.2f\n", robot.getX(), robot.getY(), robot.getTh(), robot.getVel(), robot.getRotVel());
+	// 	ArUtil::sleep(300);
+	// }
 
 	Aria::shutdown();
 	Aria::exit(0);
